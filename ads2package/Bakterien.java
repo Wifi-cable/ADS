5c79ba054262a39da74f24e674441de57f7da740
mary <Wifi-cable@users.noreply.github.com>
2016-04-24 18:32:06 +0200

    for, while and if added. bugs in variables

--- ads2package/Bakterien.java
+++ ads2package/Bakterien.java
@@ -1,14 +1,19 @@
 package ads2package;
 
 public class Bakterien {
-	int stunde= 1;
-	int bg=1;	//Bacterinen gesamt
-	int bt=0;	// Bactierien teilbar 
-	int bn=0;{//backterien neu
+	static int stunde= 1;
+	static int bg=1;	//Bacterinen gesamt
+	static int bt=0;	// Bactierien teilbar 
+	static int bn=0;{//backterien neu
 		
 	if (bg==1||bg==2){ // wenn die gesamt zahl 1 oder 2 ist die neue zahl 1 oder null.
 		bn=1;}	
-	
-	
+	while(stunde<=15){
+		for ( int zeit=0; zeit<16; zeit++){
+			int bg= bt+bg;
+			bn=bg;
+		}
+	}
+System.out.println( "Zur stunde "+stunde+" gibt es "+bg+" Bakterien." );	
 }
 }
