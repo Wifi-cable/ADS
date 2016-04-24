5bd73bb13000fbaf658439a7f12637a74ee50d63
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-24 13:57:28 -0700

    src/ads2package/Bakterien.java : clean up variable scopes
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/ads2package/Bakterien.java
+++ src/ads2package/Bakterien.java
@@ -1,6 +1,7 @@
 package ads2package;
 
-public class Bakterien {
+public class Bakterien 
+{
     int bt= 0;   // teilbare Bakterien
     int bn= 1;   // neue Bakterien
     
@@ -20,12 +21,10 @@ public class Bakterien {
         int stunde= 0;
         double ratio= 10*precision;
         double oldratio;
-        int bg;
-        bg= bt+bn;
         do
         {
             oldratio= ratio;
-            bg= bt+bn;  // Gesamtzahl der Bakterien
+            int bg= bt+bn;  // Gesamtzahl der Bakterien
             ratio= 1.0*bg/bt;
             bn= bt;
             bt= bg;
