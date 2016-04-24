bd9b2f81e61ffe7bbe6dd3a98736078670bf585d
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-24 12:08:51 -0700

    src/ads2package Main.java, Bakterien.java debugged
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/ads2package/Bakterien.java
+++ src/ads2package/Bakterien.java
@@ -1,19 +1,17 @@
 package ads2package;
 
 public class Bakterien {
-	static int stunde= 1;
-	static int bg=1;	//Bacterinen gesamt
-	static int bt=0;	// Bactierien teilbar 
-	static int bn=0;{//backterien neu
-		
-	if (bg==1||bg==2){ // wenn die gesamt zahl 1 oder 2 ist die neue zahl 1 oder null.
-		bn=1;}	
-	while(stunde<=15){
-		for ( int zeit=0; zeit<16; zeit++){
-			int bg= bt+bg;
-			bn=bg;
-		}
-	}
-System.out.println( "Zur stunde "+stunde+" gibt es "+bg+" Bakterien." );	
-}
+    static int bt= 0;   // teilbare Bakterien
+    static int bn= 1;   // neue Bakterien
+    
+    public void incubus(int endzeit)
+    {
+        for (int stunde= 1; stunde<=endzeit; stunde++)
+        {
+            int bg= bt+bn;  // Gesamtzahl der Bakterien
+            bn= bt;
+            bt= bg;
+            System.out.println( "Zur Stunde "+stunde+" gibt es "+bg+" Bakterien." ); 
+        }
+    }
 }
--- src/ads2package/MainAdsUebung2.java
+++ src/ads2package/MainAdsUebung2.java
@@ -1,10 +1,9 @@
 package ads2package;
 
 public class MainAdsUebung2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    Bakterien inkubator1;
+	    inkubator1 = new Bakterien();
+	    inkubator1.incubus(15);
 	}
-
 }
