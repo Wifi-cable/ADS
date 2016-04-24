df291d50098fa83cf7daf86ca0620b7fd77f8707
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-24 15:44:44 -0700

    Suche.java : 2 versions, with and without recursion. Not sure if either is what was asked.
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/ads2package/Suche.java
+++ src/ads2package/Suche.java
@@ -24,15 +24,39 @@ public class Suche {
         else if (anfang == ende)
         {
             System.out.println(key+ " nicht gefunden");
-            return -1;
+            return 1<<31;
         }
         else if (key<mitte)
         {
-            return suche(key, anfang, mitte);
+            return suche(key, anfang, mitte-1);
         }
         else 
         {
-            return suche(key, mitte, ende);
+            return suche(key, mitte+1, ende);
         }
 	}
+    // Alternative Version ohne Rekursion
+    int suche(boolean b)
+    {
+        System.out.println("Bitte eingeben zu wollen die Zahl zu suchen. ");
+        int key=in.nextInt(); 
+        int anfang=1;
+        int ende =10; // not recursive do wile key!=mitte?
+        int mitte=(anfang+ende)>>1;
+        while (key != mitte)
+        {
+            if (anfang == ende)
+            {
+                System.out.println(key+ " nicht gefunden");
+                return 1<<31;
+            }
+            if (key<mitte)
+                ende= mitte-1;
+            else
+                anfang= mitte+1;
+            mitte=(anfang+ende)>>1;
+        }
+        System.out.println(key+ " gefunden");
+        return mitte;
+    }
 }
