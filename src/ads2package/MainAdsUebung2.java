aaa9d4f56e38e09236dc9ae8499de522ec3683aa
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2016-04-24 15:48:51 -0700

    src/ads2package/MainAdsUebung2.java: replace \t by 4 spaces
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- src/ads2package/MainAdsUebung2.java
+++ src/ads2package/MainAdsUebung2.java
@@ -1,18 +1,18 @@
 package ads2package;
 
 public class MainAdsUebung2 {
-	public static void main(String[] args) 
-	{
-	    int endtime = 15;  // use scanner instead?
-	    System.out.println("Inkubator mit fester Inkubationszeit "+ endtime + " Stunden");
-	    Bakterien inkubator1;
-	    inkubator1 = new Bakterien();
-	    inkubator1.incubus(endtime);
+    public static void main(String[] args) 
+    {
+        int endtime = 15;  // use scanner instead?
+        System.out.println("Inkubator mit fester Inkubationszeit "+ endtime + " Stunden");
+        Bakterien inkubator1;
+        inkubator1 = new Bakterien();
+        inkubator1.incubus(endtime);
 
-	    double precision= 0.00001;
-	    System.out.println("\nInkubator zur Bestimmung der stündlichen Wachstumsrate mit Genauigkeit "+precision);
-	    Bakterien inkubator2;
-	    inkubator2 = new Bakterien();
-	    inkubator2.incubus(precision);
-	}
+        double precision= 0.00001;
+        System.out.println("\nInkubator zur Bestimmung der stündlichen Wachstumsrate mit Genauigkeit "+precision);
+        Bakterien inkubator2;
+        inkubator2 = new Bakterien();
+        inkubator2.incubus(precision);
+    }
 }
